4cff2f7553a62a787cf31567007054672ccb47f0
MKhasanov <ploffi@yandex.ru>
2017-12-25 00:06:43 +0400

    Подправил webpack, есть проблемы с hot reload ( его нет) и production сборкой - material-ui-next собирается вся включая ненужные зависимости, хз как лечить

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,77 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+const output_directory = "build";
+
+var plugins = [
+    new CleanWebpackPlugin([output_directory]),
+    new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement',
+        template: 'index.template.ejs'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js',
+    }),
+];
+
+if (isProduction) {
+    plugins = plugins.concat([
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"',
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            parallel: true,
+            uglifyOptions: {
+                ie8: false,
+            }
+        })
+    ]);
+};
 
 
 module.exports = {
-    entry:  [
-        "react-hot-loader/patch",
-        "./src/index.tsx",
-    ],
+    entry: {
+        vendor: ['react', 'react-dom', 'material-ui-next'],
+        app: ["./src/index.tsx"],
+    },
+
     output: {
         filename: "bundle.js",
-        path: __dirname + "/build"
+        path: __dirname + '/' + output_directory
     },
 
     devServer: {
         contentBase: './dist',
-        hot: true
     },
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", '.jsx', ".json"]
     },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Hot Module Replacement',
-          template: 'index.template.ejs'
-        }),
-       new webpack.HotModuleReplacementPlugin()
-      ],
+    plugins: plugins,
 
     module: {
-        rules: [
-            { 
-                test: /\.tsx?$/, 
-                loaders: [
-                    "awesome-typescript-loader", 
-                ] 
+        rules: [{
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react']
+                },
+                exclude: /node_modules/
             },
-            { test: /\.css$/,  use: [ 'style-loader', 'css-loader' ]},
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            {
+                test: /\.tsx?$/,
+                loaders: [
+                    "awesome-typescript-loader",
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
         ]
     },
 };
